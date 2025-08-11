6568e4c37919677fca01ac946ada7a1453de0009
torinmo <torinmo.work@gmailcom>
2025-08-11 05:25:26 +0100

    2

--- sophi-v2-backend/medusa-config.ts
+++ sophi-v2-backend/medusa-config.ts
@@ -11,6 +11,13 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    databaseDriverOptions: {
+
+      ssl: false,
+
+      sslmode: "disable",
+
+    },
   }
 })
